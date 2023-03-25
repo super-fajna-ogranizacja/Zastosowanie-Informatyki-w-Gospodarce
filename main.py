be45e906f4814963da53107b87f384c5608afe70
Mateusz Marciniec <mateuszmar2@gmail.com>
2023-03-25 21:55:48 +0100

    Add basic discussion body parsing
    It was assumed that app attributes will be bold,
    so discussion should look like this:
    
    **Description**: ...
    **URLs**: ...
    **Types**: ...
    **Platforms**: ...
    
    ---
    <Comments>
    ...
    ...
    
    Only comments can be multiline,
    additional lines for other entries will be ignored.
    
    Error will be printed for discussions not conforming to the format!

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import json
 import os
-import requests
+import re
+
+try:
+    import requests
+except ImportError:
+    raise ImportError("Please install the required packages: requests")
 
 
+COLUMNS = 105
 owner, repo_name = os.environ["OWNER_AND_REPO"].split("/")
 
 url = "https://api.github.com/graphql"
@@ -28,20 +34,99 @@ variables = {
     "owner": owner,
 }
 
+
 def apply_variables(query, variables):
+    """Replaces the variables names in the query with their values
+
+    Parameters
+    ----------
+    query : str
+        The query to be sent to the server
+    variables : dict
+        The variables to be replaced in the query
+
+    """
+
     for name, value in variables.items():
         query = query.replace("$"+name, str(value))
 
     return query
+
+
+def parse_discussions(response):
+    """Gets discussions from server's response and prints them
+
+    Parameters
+    ----------
+    response : requests.Response object
+        Contains the server's response to the HTTP request.
+
+    """
+
+    # Regexes for parsing the entries in discussion body
+    # multiline_re is needed to match even if there are new lines
+    multiline_re = r"[\s\S]*"
+    description_re = r"\*\*Description\*\*: (.*)" + multiline_re
+    url_re = r"\*\*URLs\*\*: (.*)\n" + multiline_re
+    types_re = r"\*\*Types\*\*: (.*)\n" + multiline_re
+    platforms_re = r"\*\*Platforms\*\*: (.*)\n" + multiline_re
+    # Additional lines are considered only in comments section
+    comments_re = r"\s*---\s*" + r"(" + multiline_re + r")"
+
+    body_regex = re.compile(description_re + url_re +
+                            types_re + platforms_re + comments_re + r"$")
+
+    print("Printing fetched and parsed discussions:")
+    for discussion in json.loads(response.text)["data"]["repository"]["discussions"]["nodes"]:
+        app_name = discussion["title"]
+        matched = body_regex.match(discussion["body"])
+        printAppDetails(app_name, matched)
+
+    print('*' * COLUMNS)
+
+
+def printAppDetails(app_name, matched):
+    """Prints the details of an app
+
+    Parameters
+    ----------
+    app_name : str
+        Name of the app
+    matched : re.Match object
+        Contains the matched groups of the regex
+    """
+
+    print('-' * COLUMNS)
+
+    try:
+        description = matched.group(1)
+        urls = matched.group(2)
+        types = matched.group(3)
+        platforms = matched.group(4)
+        comments = matched.group(5)
+    except (AttributeError):
+        print(f'Error during body parsing for {app_name}')
+        return
+
+    print(f'App_name: {app_name}',
+          f'Description: {description}',
+          f'Urls: {urls}',
+          f'Types: {types}',
+          f'Platforms: {platforms}',
+          f'Comments: {comments}',
+          sep='\n'
+          )
+
 
 api_token = os.environ["API_TOKEN"]
 headers = {
     "Authorization": "Bearer " + api_token,
 }
 
-r = requests.post(
+response = requests.post(
     url=url,
     json={"query": apply_variables(query, variables)},
     headers=headers,
 )
-print(json.dumps(json.loads(r.text), indent=4))
+
+parse_discussions(response)
