af579e8a8de1bbd430f0f22d31bb03e137b534ca
Rafał Rodak <rafalrodak00@gmail.com>
2023-05-27 18:42:26 +0200

    Add filtering with corresponding messages

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 class VirtualScroll {
     rows = [];
     container = null;
+    innerContainer = null;
     options = {};
     defaultOptions = {
         rowHeight: 24,
@@ -40,6 +41,11 @@ class VirtualScroll {
         throw new Error(`Invalid option: ${name}`);
     }
 
+    setRows(rows) {
+        this.rows = rows;
+        this.mount();
+    }
+
     getStartNode = (scrollTop) => {
         const startNode = Math.floor(scrollTop / this.getOption('rowHeight')) - this.getOption('rowPadding');
         return Math.max(0, startNode);
@@ -51,9 +57,9 @@ class VirtualScroll {
     }
 
     render(isFirst = false) {
-        const {container: {firstElementChild, scrollTop}, rows} = this;
+        const {container: {scrollTop}, rows} = this;
 
-        if (!firstElementChild) {
+        if (!this.innerContainer) {
             return;
         }
 
@@ -62,11 +68,7 @@ class VirtualScroll {
         const offsetY = startNode * this.getOption('rowHeight');
 
         if (!isFirst) {
-            firstElementChild.childNodes.forEach((node, index) => {
-                if (index < startNode || index >= startNode + visibleNodesCount ) {
-                    node.remove();
-                }
-            });
+            this.unmountItems();
         } else {
             Array.from({length: visibleNodesCount}, (_, i) => this.renderItem(i));
         }
@@ -79,6 +81,7 @@ class VirtualScroll {
     }
 
     renderItem = (index) => {
+        this.unmountMessage();
         const data = {
             rowHeight: this.getOption('rowHeight'),
             text: this.rows[index].name,
@@ -86,22 +89,84 @@ class VirtualScroll {
         }
         let row = this.cachedRows.get(index);
         if (!row) {
-            row = new VirtualRow(this.container.firstElementChild, data);
+            row = new VirtualRow(this.innerContainer, data);
             this.cachedRows.set(index, row);
         }
         return row;
     }
 
+    unmountItems = () => {
+        if (this.innerContainer) {
+            Array.from(this.innerContainer.childNodes).forEach(node => node.remove());
+        }
+    }
+
+    renderMessage = (text) => {
+        const {container} = this;
+        let messageContainer = container.querySelector('.message-container');
+        if (messageContainer) {
+            container.querySelector('.message-container').innerText = text;
+            return;
+        }
+
+        messageContainer = document.createElement('div');
+        messageContainer.innerText = text;
+        messageContainer.classList.add('message-container');
+
+        if (this.innerContainer){
+            container.replaceChild(messageContainer, this.innerContainer);
+        } else {
+            container.appendChild(messageContainer);
+        }
+    }
+
+    unmountMessage = () => {
+        const messageContainer = document.querySelector('.message-container');
+        if (messageContainer) {
+            if (this.innerContainer){
+                this.container.replaceChild(this.innerContainer, messageContainer);
+            } else {
+                container.appendChild(document.createElement('div'));
+                this.innerContainer = container.firstElementChild;
+            }
+            this.innerContainer.classList.remove("message-container")
+            Object.assign(this.innerContainer.style, {
+                height: `${this.getOption('height')}px`,
+                overflow: 'hidden',
+            })
+        }
+    }
+
     mount = () => {
-        const {container, rows} = this;
-        this.setOption('height', rows.length * this.getOption('rowHeight'))
+        const {container, rows : {length}} = this;
+
+        if (!container) {
+            return;
+        }
+
+        this.cachedRows.clear();
+        this.setOption('height', length * this.getOption('rowHeight'))
         this.setOption('visibleHeight', container.clientHeight)
         container.style.visibility = "visible";
 
-        if (!container.firstElementChild) {
-            container.appendChild(document.createElement('div'));
+        if (length > this.getOption('rowMaxNumber')) {
+            this.renderMessage(`The maximum number of rows is ${this.getOption('rowMaxNumber')}`);
+            return;
         }
-        Object.assign(container.firstElementChild.style, {
+        if (length <= 0) {
+            this.renderMessage('No elements to display');
+            return;
+        }
+
+        this.unmountMessage();
+        if (!this.innerContainer) {
+            container.appendChild(document.createElement('div'));
+            this.innerContainer = container.firstElementChild;
+        } else {
+            this.unmountItems();
+        }
+
+        Object.assign(this.innerContainer.style, {
             height: `${this.getOption('height')}px`,
             overflow: 'hidden',
         })
@@ -110,10 +175,11 @@ class VirtualScroll {
     }
 
     unmount() {
-        const {container} = this;
-        container.firstElementChild.remove();
-        container.style.visibility = "hidden";
-        this.cachedRows.clear();
+        if (this.innerContainer){
+            this.innerContainer.remove();
+            this.innerContainer = null;
+        }
+        this.container.style.visibility = "hidden";
     }
 }
 
@@ -136,13 +202,17 @@ class VirtualRow {
 
     render = (data) => {
         const rowContainer = document.createElement('p');
+        const rowInner = document.createElement('a');
         Object.assign(rowContainer.style, {
             height: `${data.rowHeight}px`,
             overflow: 'hidden',
             marginBottom: 0
         })
-        rowContainer.innerText = data.text;
         rowContainer.title = data.title;
+        rowInner.href = data.url;
+        rowInner.target = '_blank';
+        rowInner.innerText = data.text;
+        rowContainer.appendChild(rowInner);
         this.element = rowContainer;
         return this.element;
     }
@@ -152,26 +222,87 @@ class VirtualRow {
     }
 }
 
+class SearchPrompt {
+    title = null;
+    urls = null;
+    type = null;
+    platform = null;
+    desc = null;
+    comment = null;
+
+    constructor(text) {
+        this.title = text.toLowerCase();
+    }
+
+    matches(project) {
+        return project.name.toLowerCase().includes(this.title);
+    }
+}
+
+class SearchBox {
+    constructor(searchBoxElement, delay = 300, callback) {
+        this.searchBox = searchBoxElement;
+        this.delay = delay;
+        this.callback = callback;
+
+        // Bind the event listener
+        this.searchBox.addEventListener('input', this.debounce(this.handleSearchInput.bind(this), this.delay));
+    }
+
+    debounce(func, timeout = 300){
+        let timer;
+        return (...args) => {
+            clearTimeout(timer);
+            timer = setTimeout(() => { func.apply(this, args); }, timeout);
+        };
+    }
+
+    handleSearchInput() {
+        if (typeof this.callback === 'function') {
+            this.callback(this.searchBox.value);
+        } else {
+            throw new Error('No callback function provided');
+        }
+    }
+}
+
 const container = document.getElementById('virtualScroll');
 const searchBox = document.getElementById('searchBox');
 
 window.addEventListener("load", () => {
     const rows = Array.from({length: 100}, (_, i) =>
-        ({id: i, name: `Item ${i}`})
+        ({id: i, name: `Item ${i}`, url: "https://www.google.com"})
     );
+
+    const filterProjects = (promptText, projects) => {
+        const search = new SearchPrompt(promptText);
+
+        return projects.filter(p => search.matches(p));
+    };
+
+    const callbackFunction = (searchInput) => {
+        const projects = filterProjects(searchInput, rows);
+        virtualScroll.setRows(projects);
+    };
+
     const virtualScroll = new VirtualScroll(container, rows, {});
+    new SearchBox(searchBox, 300, callbackFunction);
 
     searchBox.addEventListener('focus', () => {
         virtualScroll.mount();
     });
 
-    searchBox.addEventListener('blur', () => {
-        // virtualScroll.unmount();
+    document.addEventListener('click', (event) => {
+        const target = event.target;
+        if (!container.contains(target) && target !== searchBox) {
+            virtualScroll.unmount();
+        }
     });
 
     container.addEventListener('scroll', () => {
         virtualScroll.render();
     });
 
-    console.log("page is fully loaded");
+
+    console.info("Page is fully loaded");
 });
