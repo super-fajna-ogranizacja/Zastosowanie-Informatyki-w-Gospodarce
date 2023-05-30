fbe8f8797443128aeb855908dc49b39cd8f7715d
Piotr Rodziewicz <piotr.rodziewicz06@gmail.com>
2023-05-30 15:40:21 +0200

    Delete blank line in project.html.mustache

--- tests/test_html_generation.sh
+++ tests/test_html_generation.sh
@@ -4,18 +4,14 @@
 
 python yaml2html.py
 
-cat ./html/czkawka.html
-
-cat ./html/just.html
-
-if ! cmp ./html/czkawka.html ./testfile1.html;then
+if ! cmp -s ./html/czkawka.html ./testfile1.html;then
     echo "test1 failed"
     exit 1
 else
     echo "test1 passed"
 fi
 
-if cmp ./html/just.html ./testfile2.html;then
+if cmp -s ./html/just.html ./testfile2.html;then
     echo "test2 failed"
     exit 1
 else
