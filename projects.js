bc24461039f0c1be195f0d404e757c38a418bdb8
michalsieron <54780569+michalsieron@users.noreply.github.com>
2023-05-31 19:43:43 +0000

    Deploying to gh-pages from @ super-fajna-ogranizacja/Zastosowanie-Informatyki-w-Gospodarce@45ba0e9afe99483141ebb930fc53331b9a7248bf 🚀

---
+++ projects.js
@@ -0,0 +1,65 @@
+export const projects = {
+  "TinyNvidiaUpdateChecker": {
+    "description": "check for NVIDIA GPU driver updates",
+    "urls": [
+      "https://github.com/ElPumpo/TinyNvidiaUpdateChecker"
+    ],
+    "types": [
+      "GUI"
+    ],
+    "platforms": [
+      "Windows"
+    ],
+    "categories": "Using now and then",
+    "comments": "Used once, I think and looked OK. Lists [EnvyUpdate](https://github.com/fyr77/EnvyUpdate) and [nvidia-update](https://github.com/ZenitH-AT/nvidia-update) as alternatives"
+  },
+  "czkawka": {
+    "description": "a simple, fast and free app to remove unnecessary files from your computer",
+    "urls": [
+      "https://github.com/qarmin/czkawka"
+    ],
+    "types": [
+      "CLI",
+      "GUI"
+    ],
+    "platforms": [
+      "Linux",
+      "Windows",
+      "MacOS"
+    ],
+    "categories": "Want to or may use",
+    "comments": "Looks useful"
+  },
+  "hyperfine": {
+    "description": "a command-line benchmarking tool",
+    "urls": [
+      "https://github.com/sharkdp/hyperfine"
+    ],
+    "types": [
+      "CLI"
+    ],
+    "platforms": [
+      "Linux",
+      "Windows",
+      "MacOS"
+    ],
+    "categories": "Currently using",
+    "comments": "great for comparing different commands"
+  },
+  "just": {
+    "description": "a handy way to save and run project-specific commands",
+    "urls": [
+      "https://github.com/casey/just"
+    ],
+    "types": [
+      "CLI"
+    ],
+    "platforms": [
+      "Linux",
+      "Windows",
+      "MacOS"
+    ],
+    "categories": "Currently using",
+    "comments": "great command runner searching for justfiles in upper directories"
+  }
+};
