0018e90e3aa0347fdf6b154b5975bd0512ed43d3
Piotr Rodziewicz <piotr.rodziewicz06@gmail.com>
2023-05-31 21:53:31 +0200

    Adjust files again

--- tests/test_html_generation.sh
+++ tests/test_html_generation.sh
@@ -2,18 +2,18 @@
 
 #This test checks if html generation from yaml is working
 
-python yaml2html.py
+python yaml2html.py -d ./html/ czkawka.yml just.yml
 
-if ! cmp -s ./html/czkawka.html ./testfile1.html;then
+if diff ./html/czkawka.html ./testfile1.html; then
+    echo "test1 passed"
+else
     echo "test1 failed"
     exit 1
-else
-    echo "test1 passed"
 fi
 
-if cmp -s ./html/just.html ./testfile2.html;then
+if diff ./html/just.html ./testfile2.html; then
+    echo "test2 passed"
+else
     echo "test2 failed"
     exit 1
-else
-    echo "test2 passed"
 fi
